e6736a07801df7d6e102cdfb75196417ad1c8194
Thiago Enninger <thiagoenninger@gmail.com>
2025-06-11 08:36:08 -0300

    creating stars background, meteor and navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Navbar } from "../components/NavBar";
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen ">
-      <div></div>
-      <div></div>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
     </div>
   );
 };
