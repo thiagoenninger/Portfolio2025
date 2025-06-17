a87be188456d42e30e876b8e33d44ac824b5423e
Thiago Enninger <thiagoenninger@gmail.com>
2025-06-17 08:15:43 -0300

    creating final components, finalizing the project.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { AboutMeSection } from "../components/AboutMeSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/NavBar";
 import { ProjectsSection } from "../components/ProjectsSection";
-import { SkillsSection } from "../components/SkillsSectio";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -17,7 +19,9 @@ export const Home = () => {
         <AboutMeSection />
         <SkillsSection />
         <ProjectsSection />
+        <ContactSection />
       </main>
+      <Footer />
     </div>
   );
 };
