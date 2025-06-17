f060dedad9090e6526e50b7628db88448f183202
Thiago Enninger <thiagoenninger@gmail.com>
2025-06-16 22:04:57 -0300

    finish SkillsSection component

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,5 @@
+const projects = [{}];
+
+export const ProjectsSection = () => {
+  return <section id="projects"></section>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { AboutMeSection } from "../components/AboutMeSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/NavBar";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { SkillsSection } from "../components/SkillsSectio";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -13,6 +15,8 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutMeSection />
+        <SkillsSection />
+        <ProjectsSection />
       </main>
     </div>
   );
